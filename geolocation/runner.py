cc5a4862a3b44c545323aaad64baf67fd2a02865
Jerod Sun <Jerod.sun@live.com>
2022-05-03 10:39:25 -0400

    working geolocation implementation with runner

---
+++ geolocation/runner.py
@@ -0,0 +1,14 @@
+from geolocation_base import *
+import pandas as pd
+
+
+df = pd.read_csv("repo_owners_location_sample.csv")
+
+df["location"] = df["location"].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8').apply(str)
+
+# df = df.iloc[:250]
+
+
+for i in df["location"]:
+    result = get_country_from_location(i)
+    print('{:>50}  {:>50}'.format(i, result))
