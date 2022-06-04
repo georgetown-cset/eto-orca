cf5f718b8b823118136ae408fdf3c135b7bbe981
Jennifer Melot <jtmelot@gmail.com>
2022-06-04 17:38:40 -0400

    Load data from local jsonl and write to pickle

--- scripts/preprocess_for_website.py
+++ scripts/preprocess_for_website.py
@@ -1,14 +1,13 @@
 import argparse
 import json
 import os
-from datetime import datetime
+import pickle
 
-from google.cloud import bigquery
 from tqdm import tqdm
 
 
 def get_counts_by_month(dates):
-    months = [date.strftime("%Y-%m") for date in dates]
+    months = ["-".join(date.split("-")[:2]) for date in dates]
     counts = {}
     for month in months:
         counts[month] = counts.get(month, 0) + 1
@@ -17,56 +16,54 @@ def get_counts_by_month(dates):
     )
 
 
+def get_lines(input_dir: str):
+    for fi in os.listdir(input_dir):
+        with open(os.path.join(input_dir, fi)) as f:
+            for line in f:
+                yield json.loads(line)
+
+
 def retrieve_data(
-    input_table: str, output_repo_file: str, output_field_file: str
+    input_dir: str, output_repo_file: str, output_field_file: str
 ) -> None:
-    client = bigquery.Client()
-    query_job = client.query(f"select * from {input_table}")
-    results = query_job.result()
     seen_ids = set()
-    fmt_data = {}
+    id_to_repo = {}
     field_to_repos = {}
-    for result in tqdm(results):
-        if result["id"] in seen_ids:
+    for line in tqdm(get_lines(input_dir)):
+        if line["id"] in seen_ids:
             continue
-        seen_ids.add(result["id"])
+        seen_ids.add(line["id"])
         row = {}
-        for key in result.keys():
-            row[key] = (
-                result[key]
-                if not key.endswith("_at")
-                else datetime.strftime(result[key], "%Y-%m-%d")
-            )
+        for key in line.keys():
+            row[key] = line[key] if not key.endswith("_at") else line[key].split()[0]
         row["star_dates"] = get_counts_by_month(row.pop("star_dates"))
         row["push_dates"] = get_counts_by_month(row.pop("push_dates"))
         assert row["open_issues_count"] == row["open_issues"], row
         row.pop("open_issues_count")
-        for paper_meta in row["paper_meta"]:
+        for paper_meta in row.pop("paper_meta"):
             for field in paper_meta["fields"]:
                 field_name = field["name"]
                 if field_name not in field_to_repos:
                     field_to_repos[field_name] = []
                 field_to_repos[field_name].append(row["id"])
-        fmt_data[int(row["id"])] = row
-    with open(output_repo_file, mode="w") as f:
-        f.write(f"const repo_data={json.dumps(fmt_data)};\n\nexport default repo_data;")
-    with open(output_field_file, mode="w") as f:
-        f.write(
-            f"const field_to_repos={json.dumps(field_to_repos)};\n\nexport default field_to_repos;"
-        )
+        id_to_repo[int(row["id"])] = row
+    with open(output_repo_file, mode="wb") as f:
+        pickle.dump(id_to_repo, f)
+    with open(output_field_file, mode="wb") as f:
+        pickle.dump(field_to_repos, f)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_table", default="staging_github_metrics.website_stats")
+    parser.add_argument("--input_dir", default="gh_website_stats")
     parser.add_argument(
         "--output_repo_file",
-        default=os.path.join("github-metrics", "src", "data", "repos.js"),
+        default=os.path.join("id_to_repo.pkl"),
     )
     parser.add_argument(
         "--output_field_file",
-        default=os.path.join("github-metrics", "src", "data", "fields.js"),
+        default=os.path.join("field_to_repo.pkl"),
     )
     args = parser.parse_args()
 
-    retrieve_data(args.input_table, args.output_repo_file, args.output_field_file)
+    retrieve_data(args.input_dir, args.output_repo_file, args.output_field_file)
