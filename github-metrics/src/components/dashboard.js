991507b29b4aba46c3e055bca8e1daf45ada2948
Jennifer Melot <jtmelot@gmail.com>
2022-12-20 13:07:43 -0500

    Add tabs

--- github-metrics/src/components/dashboard.js
+++ github-metrics/src/components/dashboard.js
@@ -7,7 +7,10 @@ import FormControl from "@mui/material/FormControl";
 import Paper from "@mui/material/Paper";
 import Select from "@mui/material/Select";
 import Typography from "@mui/material/Typography";
-import { styled } from '@mui/material/styles';
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
 
 import "core-js/features/url";
 import "core-js/features/url-search-params";
@@ -30,6 +33,50 @@ const MenuProps = {
     },
   },
 };
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <div>
+          <Typography component={"div"}>{children}</Typography>
+        </div>
+      )}
+    </div>
+  );
+}
+
+const StyledTabs = styled(Tabs)({
+  "& .MuiTabs-indicator": {
+    backgroundColor: "var(--bright-blue)"
+  },
+  "& .Mui-selected": {
+    color: "var(--dark-blue)",
+    fontWeight: "bold",
+    backgroundColor: "var(--bright-blue-light)",
+  }
+});
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 const dataUrl = "https://us-east1-gcp-cset-projects.cloudfunctions.net/github-metrics/";
 
@@ -88,6 +135,7 @@ const Dashboard = () => {
   const [filterValues, setFilterValues] = React.useState({...defaultFilterValues});
   const [fields, setFields] = React.useState([]);
   const [repoData, setRepoData] = React.useState([]);
+  const [tabValue, setTabValue] = React.useState(0);
 
   const handleSingleSelectChange = (event, key) => {
     const updatedFilterValues = {...filterValues};
@@ -148,9 +196,20 @@ const Dashboard = () => {
           </ToolbarFormControl>
         </div>
       </Paper>
-      {repoData.map(repo => (
-        <RepoCard data={repo} sortOptions={sortOptions} field={filterValues["field_of_study"]}/>
-      ))}
+      <div style={{ borderBottom: 1, borderColor: "divider", position: "sticky", top: "0", zIndex: 200, backgroundColor: "white"}}>
+        <StyledTabs value={tabValue} onChange={(evt, newValue) => {setTabValue(newValue)}} aria-label="map of science tabs">
+          <Tab label="Field summary" {...a11yProps(0)} />
+          <Tab label="Repository list" {...a11yProps(1)} />
+        </StyledTabs>
+      </div>
+      <TabPanel value={tabValue} index={0}>
+        <span>This is where the summary view will go</span>
+      </TabPanel>
+      <TabPanel value={tabValue} index={1}>
+        {repoData.map(repo => (
+          <RepoCard data={repo} sortOptions={sortOptions} field={filterValues["field_of_study"]}/>
+        ))}
+      </TabPanel>
     </div>
   )
 };
