1c26fe9cf52662177c42ff1d2f060fcfbe3ace6f
Jennifer Melot <jtmelot@gmail.com>
2022-12-20 12:45:44 -0500

    Add unstyled graphs to list view

---
+++ github-metrics/src/components/graph.js
@@ -0,0 +1,79 @@
+import React, { Suspense, lazy } from "react";
+import {PlotlyDefaults} from "@eto/eto-ui-components";
+
+const colors = [
+  "#7AC4A5",
+  "#F17F4C",
+  "#15AFD0",
+  "#B42025",
+  "#839DC5",
+  "#E5BF21",
+  "#3C8786",
+  "#7C336F",
+  "#003DA6",
+  "#B53A6D",
+];
+
+const Plot = lazy(() => import('react-plotly.js'));
+const isSSR = typeof window === "undefined";
+
+const LineGraph = (props) => {
+  const {traces, title} = props;
+
+  const offset = -1;
+
+  const traceMetadata = [];
+  let maxY = -1;
+  for(const [idx, trace] of traces.entries()) {
+    const traceMaxY = Math.max(...trace.y);
+    if(traceMaxY > maxY){
+      maxY = traceMaxY;
+    }
+    traceMetadata.push({
+      x: trace.x.slice(0, offset),
+      y: trace.y.slice(0, offset),
+      name: trace.name,
+      type: "scatter",
+      mode: "lines",
+      marker: {color: colors[idx]},
+      legendgroup: trace.name
+    });
+    traceMetadata.push({
+      x: trace.x.slice(offset-1),
+      y: trace.y.slice(offset-1),
+      name: trace.name,
+      type: "scatter",
+      mode: "lines",
+      marker: {color: colors[idx]},
+      showlegend: false,
+      line: {dash: "dash"},
+      legendgroup: trace.name
+    });
+  }
+
+  const plotlyDefaults = PlotlyDefaults(maxY);
+
+  const layout = plotlyDefaults.layout;
+  layout.showlegend = traces.length > 1;
+  layout.margin = {t: 50, r: 50, b: 50, l: 50, pad: 4};
+  if(title) {
+    layout.title = title;
+  }
+
+  return (
+    <>
+      { !isSSR &&
+        <Suspense fallback={<div>Loading graph...</div>}>
+          <Plot
+            config={plotlyDefaults.config}
+            data={traceMetadata}
+            layout={layout}
+            style={{height: "450px", width: "100%"}}
+          />
+        </Suspense>
+      }
+    </>
+  );
+};
+
+export {LineGraph};
--- github-metrics/src/components/repo_card.js
+++ github-metrics/src/components/repo_card.js
@@ -6,6 +6,8 @@ import { css } from "@emotion/react";
 import "core-js/features/url";
 import "core-js/features/url-search-params";
 
+import {LineGraph} from "../components/graph";
+
 
 const styles = {
   card: css`
@@ -40,18 +42,30 @@ const RepoCard = (props) => {
     return data[sort_key];
   };
 
+  const getX = (ary) => {
+    return ary.map(elt => elt[0])
+  };
+
+  const getY = (ary) => {
+    return ary.map(elt => elt[1])
+  };
+
   return (
     <div css={styles.card}>
-     <Link href={"https://github.com/"+repo_name}>{repo_name}</Link>
+      <Link href={"https://github.com/"+repo_name}>{repo_name}</Link>
       <div>
-      {Object.keys(sortOptions).map(sortname => (
-        <Typography component={"span"} variant={"body2"} css={styles.sortOption}>
-          <span css={styles.emph}>{sortname}</span>: {getValue(sortOptions[sortname])}
+        {Object.keys(sortOptions).map(sortname => (
+          <Typography component={"span"} variant={"body2"} css={styles.sortOption}>
+            <span css={styles.emph}>{sortname}</span>: {getValue(sortOptions[sortname])}
+          </Typography>
+        ))}
+        <Typography component={"p"} variant={"body2"} css={styles.dataDesc}>
+          {data["description"]}
         </Typography>
-      ))}
-      <Typography component={"p"} variant={"body2"} css={styles.dataDesc}>
-        {data["description"]}
-      </Typography>
+      </div>
+      <div>
+        <LineGraph traces={[{x: getX(data["star_dates"]), y: getY(data["star_dates"])}]} title={"Stars over Time"}/>
+        <LineGraph traces={[{x: getX(data["push_dates"]), y: getY(data["push_dates"])}]} title={"Push Events over Time"}/>
       </div>
     </div>
   )
