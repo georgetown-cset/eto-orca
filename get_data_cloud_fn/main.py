d622da355256de8071277e2b35947994dcb87ee3
Jennifer Melot <jtmelot@gmail.com>
2022-12-30 21:20:15 -0500

    Filter results on primary programming language

--- get_data_cloud_fn/main.py
+++ get_data_cloud_fn/main.py
@@ -7,6 +7,8 @@ with open(os.path.join("data", "field_to_repo.pkl"), mode="rb") as f:
     FIELD_TO_REPOS = pickle.load(f)
 with open(os.path.join("data", "id_to_repo.pkl"), mode="rb") as f:
     ID_TO_REPO = pickle.load(f)
+with open(os.path.join("data", "languages.pkl"), mode="rb") as f:
+    LANGUAGES = pickle.load(f)
 
 
 @functions_framework.http
@@ -27,7 +29,14 @@ def get_data(request):
     if request_json and ("field" in request_json) and ("order_by" in request_json):
         field = request_json["field"]
         all_repo_ids = FIELD_TO_REPOS[field]
-        all_repos = [ID_TO_REPO[int(repo_id)] for repo_id in all_repo_ids]
+        all_repos = []
+        for repo_id in all_repo_ids:
+            repo = ID_TO_REPO[int(repo_id)]
+            language = repo["primary_programming_language"]
+            if (not "language" in request_json) or (  # noqa: E713
+                language == request_json["language"]
+            ):
+                all_repos.append(repo)
         order_by = request_json["order_by"]
         if order_by == "num_references":
             get_order = lambda r: r["num_references"][field]  # noqa: E731
@@ -35,6 +44,8 @@ def get_data(request):
             get_order = lambda r: r[order_by]  # noqa: E731
         sorted_repos = sorted(all_repos, key=lambda r: get_order(r), reverse=True)
         result = {"matches": sorted_repos[:20]}
+    elif "language" in request_json:
+        result = {"languages": sorted(list(LANGUAGES))}
     else:
         result = {"fields": sorted(list(FIELD_TO_REPOS.keys()))}
     headers = {"Access-Control-Allow-Origin": "*"}
