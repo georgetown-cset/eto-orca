cc6b3d423bfcedb92d59259c8178492d0d7c96ed
Jennifer Melot <jtmelot@gmail.com>
2023-05-30 10:41:26 -0400

    Fix pypi counts where multiple projects have the same repo
    Closes #74

--- sql/pypi_over_time.sql
+++ sql/pypi_over_time.sql
@@ -1,11 +1,27 @@
 -- get repo's pypi summary and downloads by country over time
+WITH yearly_country_counts AS (
+  SELECT
+    repo,
+    max(summary) AS summary,
+    year,
+    country_code,
+    -- multiple projects may have the same repo (e.g. if a project name changes).
+    -- When this happens, take the sum of their downloads, since these downloads
+    -- all correspond to the same overall project
+    sum(num_downloads) AS num_downloads
+  FROM
+    staging_github_metrics.pypi_repo_metadata
+  LEFT JOIN
+    staging_github_metrics.pypi_file_downloads
+    USING (project)
+  GROUP BY repo, year, country_code
+)
+
 SELECT
   repo,
   summary,
   array_agg(STRUCT(year, country_code, num_downloads)) AS downloads
 FROM
-  staging_github_metrics.pypi_repo_metadata
-LEFT JOIN
-  staging_github_metrics.pypi_file_downloads
-  USING (project)
-GROUP BY repo, summary
+  yearly_country_counts
+GROUP BY
+  repo, summary
