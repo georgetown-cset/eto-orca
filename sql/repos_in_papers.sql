b7199ff3225cdf7542cb94af5ecdac9af9d0c270
Jennifer Melot <jtmelot@gmail.com>
2023-06-08 11:35:13 -0400

    Remove CNKI

--- sql/repos_in_papers.sql
+++ sql/repos_in_papers.sql
@@ -1,52 +1,5 @@
--- get repo mentions in CNKI fulltext, arXiv fulltext, Papers with Code, and paper titles/abstracts
-WITH
-cnki_ft AS ((
-    SELECT
-      cset_cnki_journals_id_mappings.cnki_document_id AS id,
-      regexp_replace(cnki_journals_ft.full_text, r"-\s*\n", "-") AS full_text
-    FROM
-      gcp_cset_cnki.cnki_journals_ft
-    INNER JOIN
-      gcp_cset_cnki.cset_cnki_journals_id_mappings
-      -- For uniqueness, match on both document_name and cnki_doi. Unlike cnki_doi, document_name is never null.
-      -- Fall back to matching document name alone if cnki_doi is null
-      ON
-        (cset_cnki_journals_id_mappings.document_name = cnki_journals_ft.document_name)
-        AND ((cset_cnki_journals_id_mappings.cnki_doi = cnki_journals_ft.doi)
-          OR (cset_cnki_journals_id_mappings.cnki_doi IS NULL)
-          OR (trim(cset_cnki_journals_id_mappings.cnki_doi) = "")))
-  UNION ALL (
-    SELECT
-      cset_cnki_dissertations_id_mappings.cnki_document_id AS id,
-      regexp_replace(cnki_dissertations_ft.full_text, r"-\s*\n", "-") AS full_text
-    FROM
-      gcp_cset_cnki.cnki_dissertations_ft
-    INNER JOIN
-      gcp_cset_cnki.cset_cnki_dissertations_id_mappings
-      -- For uniqueness, match on both document_name and cnki_doi. Unlike cnki_doi, document_name is never null.
-      -- Fall back to matching document name alone if cnki_doi is null
-      ON
-        ( cset_cnki_dissertations_id_mappings.document_name = cnki_dissertations_ft.document_name )
-        AND ((cset_cnki_dissertations_id_mappings.cnki_doi = cnki_dissertations_ft.doi)
-          OR (cset_cnki_dissertations_id_mappings.cnki_doi IS NULL)
-          OR (trim(cset_cnki_dissertations_id_mappings.cnki_doi) = "")))
-  UNION ALL (
-    SELECT
-      cset_cnki_conferences_id_mappings.cnki_document_id AS id,
-      regexp_replace(cnki_conferences_ft.full_text, r"-\s*\n", "-") AS full_text
-    FROM
-      gcp_cset_cnki.cnki_conferences_ft
-    INNER JOIN
-      gcp_cset_cnki.cset_cnki_conferences_id_mappings
-      -- For uniqueness, match on both document_name and doi. Unlike doi, document_name is never null.
-      -- Fall back to matching document name alone if doi is null
-      ON
-        ( cset_cnki_conferences_id_mappings.document_name = cnki_conferences_ft.document_name )
-        AND ((cset_cnki_conferences_id_mappings.cnki_doi = cnki_conferences_ft.doi)
-          OR (cset_cnki_conferences_id_mappings.cnki_doi IS NULL)
-          OR (trim(cset_cnki_conferences_id_mappings.cnki_doi) = "")))),
-
-arxiv_ft AS (
+-- get repo mentions in arXiv fulltext, Papers with Code, and paper titles/abstracts
+WITH arxiv_ft AS (
   SELECT
     id,
     regexp_replace(joined_text, r"-\s*\n", "-") AS full_text
@@ -60,19 +13,12 @@ pwc AS (
   FROM
     papers_with_code.links_between_papers_and_code),
 
-pwc_arxiv_cnki AS (
+pwc_arxiv AS (
   SELECT
     merged_id,
     dataset,
     full_text
   FROM (
-    SELECT
-      id,
-      full_text,
-      "cnki" AS dataset
-    FROM
-      cnki_ft
-    UNION ALL
     SELECT
       id,
       full_text,
@@ -95,14 +41,14 @@ agg_repos AS (
   SELECT
     merged_id,
     dataset,
-    github_metrics.get_first_repo_slug(full_text) AS repos
+    github_metrics.get_all_repo_slugs(full_text) AS repos
   FROM (
     SELECT
       merged_id,
       dataset,
       full_text
     FROM
-      pwc_arxiv_cnki
+      pwc_arxiv
     UNION ALL
     SELECT
       merged_id,
